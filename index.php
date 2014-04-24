f3a2b1aa3da7c41675e89823f8905958395cdac5
jakubsindelar <github@heroesofmemory.eu>
2014-04-24 23:14:01 +0200

    Create project

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>Cloud, s. r. o. - Firemní databáse</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=9" />
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/login.css">
+    <?php include("./config.php"); ?>
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <meta name="description" content="Firemní databáse">
+    <meta name="author" content="Jakub Šindelář" />
+  </head>
+  <body>
+  <div id="layout">asdfasdfasd</div>
+  
+  <div id="footer">Cloud, s. r. o.</div>
+  </body>
+    </html>
