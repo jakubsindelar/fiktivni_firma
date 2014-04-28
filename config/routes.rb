1064f8f306355ed201ca32956a9e4d32a209f614
pdostal <git@pdostal.cz>
2014-04-28 21:49:21 +0200

    Fix root_path

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   resources :employees
 
 
-  get '', to: 'home#index', as: 'home' 
-
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
