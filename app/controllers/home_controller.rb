f7cf95e9b57b064a0c604d7cc2e90e9baf3009a2
jakubsindelar <github@heroesofmemory.eu>
2014-04-30 18:29:27 +0200

    Add some links to menu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about # vytvořen ínového view
+  end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :employees
 
+  get 'about' => 'home#about'
+ 
 
 
   # The priority is based upon order of creation: first created -> highest priority.
