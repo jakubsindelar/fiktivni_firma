6ceeb93b0968d2669974b437634f2a4fc9a8004b
jakubsindelar <github@heroesofmemory.eu>
2014-04-30 18:34:36 +0200

    Generate mail scaffold

---
+++ db/migrate/20140430163431_create_contacts.rb
@@ -0,0 +1,11 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :subject
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
