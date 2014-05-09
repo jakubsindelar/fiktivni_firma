74373b1c86ad61959462653144746b70f7b6cb45
jakubsindelar <github@heroesofmemory.eu>
2014-05-09 20:55:07 +0200

    Added some forgoten translations to employes

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -28,7 +28,7 @@ class EmployeesController < ApplicationController
 
     respond_to do |format|
       if @employee.save
-        format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
+        format.html { redirect_to @employee, notice: 'Nový zaměstnanec byl úzpěšně vytvořen.' }
         format.json { render :show, status: :created, location: @employee }
       else
         format.html { render :new }
