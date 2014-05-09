5e3dfe127c44542231c0e6da8d879aeacc96d879
jakubsindelar <github@heroesofmemory.eu>
2014-05-09 19:45:14 +0200

    edit and add coloumn and edit text

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -69,6 +69,6 @@ class EmployeesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def employee_params
-      params.require(:employee).permit(:name, :surname, :address, :social_security_number, :birth_date)
+      params.require(:employee).permit(:name, :surname, :street, :social_security_number, :birth_date, :city, :post_number)
     end
 end
