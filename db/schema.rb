18cc3e92aa3ed3c6294674396d7ba1f987641562
jakubsindelar <github@heroesofmemory.eu>
2014-05-18 20:10:35 +0200

    added scafold product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140518153853) do
+ActiveRecord::Schema.define(version: 20140518180959) do
 
   create_table "clients", force: true do |t|
     t.string   "name"
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 20140518153853) do
     t.datetime "updated_at"
     t.string   "city"
     t.decimal  "post_number",            precision: 10, scale: 0
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.integer  "size"
+    t.integer  "account"
+    t.integer  "backup"
+    t.integer  "amount"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "services", force: true do |t|
