bbdbb13a09275b281d093918578e43c07f5fe25e
jakubsindelar <github@heroesofmemory.eu>
2014-07-12 23:13:27 +0200

    přidány firmy a odkaz na ně

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :customers
 
+ get 'companies'  => 'home#companies'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
